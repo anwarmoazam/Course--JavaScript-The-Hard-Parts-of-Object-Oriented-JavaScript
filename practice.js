821ab49fc25d99c1c4e4d592bc88ef812ab0b64c
unknown <you@example.com>
2022-02-25 19:26:46 +0530

    practice

--- practice.js
+++ practice.js
@@ -77,8 +77,14 @@ function UserCreator(name,score){
     this.score = score;
 }
 
+// UserCreator.prototype.increament = function(){
+//     this.score++;
+// }
+
+// We need to introduce arrow function - which bind this lexically
 UserCreator.prototype.increament = function(){
-    this.score++;
+    const add1 = ()=>{this.score++};
+    add1();
 }
 
 UserCreator.prototype.login = function(){
@@ -86,6 +92,74 @@ UserCreator.prototype.login = function(){
 }
 
 const user7 = new UserCreator("Ankit Mathur", 20);
+console.log(user7.score);
 user7.increament();
-console.log(user7);
+console.log(user7.score);
 console.log(user7.login());
+
+
+// Creating Object with Class
+class UserCreatorWithClass{
+    constructor(name,score){
+        this.name = name;
+        this.score = score;
+    }
+    increament(){
+        this.score++;
+    }
+    login(){
+        console.log(`${this.name} is logged in`);
+    }
+}
+
+const user8 = new UserCreatorWithClass('Nirbhay', 5);
+console.log(user8.name);
+user8.login();
+user8.increament();
+
+console.log(user8.score);
+console.log(user8.hasOwnProperty('score'));
+console.log(UserCreator.prototype);
+
+// Subclassing in Solution 2
+
+// Creating normal user
+function userCreator1(name,score){
+    const newUser = Object.create(userFunctions);
+    newUser.name = name;
+    newUser.score = score;
+    return newUser;
+}
+
+userFunctions = {
+    sayName: function(){
+        console.log(`I am ${this.name}`);
+    },
+    increament: function(){
+        this.score++;
+    }
+}
+
+const user9 = userCreator1('Mohammad Qamil', 9);
+console.log(user9);
+user9.sayName();
+
+
+// Creating paid user
+function paidUserCreator(paidName, paidScore, accountBalance){
+    const newPaidUser = userCreator1(paidName,paidScore);
+    Object.setPrototypeOf(newPaidUser,paidUserFuntions);
+    newPaidUser.accountBalance = accountBalance;
+    return newPaidUser;
+}
+
+const paidUserFuntions = {
+    increaseBalance: function(){
+        this.accountBalance++;
+    }
+};
+
+Object.setPrototypeOf(paidUserFuntions,userFunctions);
+
+const paidUser1 = paidUserCreator('Sana',3, 9);
+console.log(paidUser1);
